3e7b3b7b31a4b3f5e2c0b1c113ef6f08c71be8b9
AivinJoy <aivincjoy@gmail.com>
2026-01-26 22:06:29 +0530

    update api url to production.

--- sales-ui/src/lib/config.ts
+++ sales-ui/src/lib/config.ts
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
+export const API_URL = "https://sales-automation-ynh9.onrender.com";
